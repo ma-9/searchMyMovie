40fddacc3530b3fc3662a8fcd3b4b9f7464f9290
manavOza <manavoza99@gmail.com>
2019-07-25 07:16:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#ck1').click(function(){
+        if($(this).prop("checked") == true){
+            alert("Checkbox is checked.");
+        }
+        else if($(this).prop("checked") == false){
+            alert("Checkbox is unchecked.");
+        }
+    });
+});
